285baf587764a0a47a8093a5773c01b3071839d8
Sanghyun Yi <njsharry32@gmail.com>
2024-04-08 16:38:30 +0900

    styling app theme and setting

--- app/src/main/java/android/dev/compose/MainActivity.kt
+++ app/src/main/java/android/dev/compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package android.dev.compose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.dev.compose.ui.theme.JetpackComposeTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -25,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -119,7 +121,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ))
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -129,6 +134,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
